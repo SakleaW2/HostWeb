56ebf4777cc83fb78d17dad483a345d3c5259063
SakleaW2 <merbogdanodincov5@gmail.com>
2026-03-08 11:49:31 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+/* ================= ГОД В FOOTER ================= */
+
+document.getElementById("year").textContent = new Date().getFullYear();
+
+/* ================= КНОПКА ВВЕРХ ================= */
+
+const topBtn = document.getElementById("topBtn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 400) {
+    topBtn.style.display = "block";
+  } else {
+    topBtn.style.display = "none";
+  }
+});
+
+topBtn.onclick = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
+
+/* ================= ФОРМА ================= */
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  document.getElementById("status").textContent = "Заявка отправлена!";
+}
